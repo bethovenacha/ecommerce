fdb2f01768d5ef0355db2a5741ae4f5346bd9a7f
BethovenAcha <bmacha2019@gmail.com>
2025-09-11 14:06:47 +0800

    using withLatestFrom method to get data from Store

--- angular-client/admin/src/app/store/order/order.effect.ts
+++ angular-client/admin/src/app/store/order/order.effect.ts
@@ -1,18 +1,26 @@
 import { Inject, inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { orderCreate } from "./order.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
+import { Store } from "@ngrx/store";
+import { OrderState } from "./order.state";
 
 // Effect to handle side effects for order actions
 // if orderCreate is dispatched, log the order and save to localStorage
 @Injectable()
 export class OrderEffect {
     private orderActions$ = inject(Actions)
+    private store = inject(Store<{orderState: OrderState}>);
     saveOrder = createEffect(() => this.orderActions$.pipe(
         ofType(orderCreate),
-        tap((action)=>{
-            console.log("Order created:", action.value.status.toString());
-            localStorage.setItem('lastOrder', JSON.stringify(action.value));
+        withLatestFrom(this.store.select(state => state.orderState.orders)), // get latest orders from state
+        tap(([action,state])=>{
+           // console.log("Order created:", action.value.status.toString());
+           // localStorage.setItem('lastOrder', JSON.stringify(action.value));
+            localStorage.setItem("orders", JSON.stringify(state));
+            var orders = localStorage.getItem("orders");
+            console.log("Order created:", JSON.stringify(orders));
+         
         })
        // ,  map((action) => orderCreatedSuccess({ id: action.id })) // 👈 must emit an Action
     ), { dispatch: false });
