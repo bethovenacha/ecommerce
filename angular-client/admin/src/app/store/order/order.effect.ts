81a08fa04b4c2ae49799e111b1332fa0f28dfe35
BethovenAcha <bmacha2019@gmail.com>
2025-09-11 13:49:36 +0800

    adding injectable

--- angular-client/admin/src/app/store/order/order.effect.ts
+++ angular-client/admin/src/app/store/order/order.effect.ts
@@ -1,8 +1,11 @@
-import { inject } from "@angular/core";
+import { Inject, inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { orderCreate } from "./order.actions";
 import { tap } from "rxjs";
 
+// Effect to handle side effects for order actions
+// if orderCreate is dispatched, log the order and save to localStorage
+@Injectable()
 export class OrderEffect {
     private orderActions$ = inject(Actions)
     saveOrder = createEffect(() => this.orderActions$.pipe(
